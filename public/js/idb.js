641a816a1cf09aacf9ecbf760083d5e1390ed424
joshgit <jschermann91@gmail.com>
2022-02-15 16:43:20 -0600

    indexdb implemented

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,4 @@
 let db;
-
 const request = indexedDB.open("quick-budget", 1);
 
 // this event will emit if the database version changes (nonexistant to
@@ -12,76 +11,77 @@ request.onupgradeneeded = function (event) {
   db.createObjectStore("new_budget", { autoIncrement: true });
 };
 
-// // upon a successful
-// request.onsuccess = function (event) {
-//   // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-//   db = event.target.result;
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) 
+// or simply established a connection, save reference to db in global variable
+  db = event.target.result;
 
-//   // check if app is online, if yes run uploadPizza() function to send all local db data to api
-//   if (navigator.onLine) {
-//     uploadPizza();
-//   }
-// };
+  // check if app is online, if yes run uploadBudget() function to send all local db data to api
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
 
-// request.onerror = function (event) {
-//   // log error here
-//   console.log(event.target.errorCode);
-// };
+request.onerror = function (event) {
+  // log error here
+  console.log(event.target.errorCode);
+};
 
-// // This function will be executed if we attempt to submit a new pizza and there's no internet connection
-// function saveRecord(record) {
-//   // open a new transaction with the database with read and write permissions
-//   const transaction = db.transaction(["new_pizza"], "readwrite");
+// This function will be executed if we attempt to submit a new pizza and there's no internet connection
+function saveRecord(record) {
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["new_budget"], "readwrite");
 
-//   // access the object store for `new_pizza`
-//   const pizzaObjectStore = transaction.objectStore("new_pizza");
+  // access the object store for `new_budget`
+  const budgetObjectStore = transaction.objectStore("new_budget");
 
-//   // add record to your store with add method
-//   pizzaObjectStore.add(record);
-// }
+  // add record to your store with add method
+  budgetObjectStore.add(record);
+}
 
-// function uploadPizza() {
-//   // open a transaction on your db
-//   const transaction = db.transaction(["new_pizza"], "readwrite");
+function uploadBudget() {    
+  // open a transaction on your db
+  const transaction = db.transaction(["new_budget"], "readwrite");
 
-//   // access your object store
-//   const pizzaObjectStore = transaction.objectStore("new_pizza");
+  // access your object store
+  const budgetObjectStore = transaction.objectStore("new_budget");
 
-//   // get all records from store and set to a variable
-//   const getAll = pizzaObjectStore.getAll();
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
 
-//   // upon a successful .getAll() execution, run this function
-//   getAll.onsuccess = function () {
-//     // if there was data in indexedDb's store, let's send it to the api server
-//     if (getAll.result.length > 0) {
-//       fetch("/api/pizzas", {
-//         method: "POST",
-//         body: JSON.stringify(getAll.result),
-//         headers: {
-//           Accept: "application/json, text/plain, */*",
-//           "Content-Type": "application/json",
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then((serverResponse) => {
-//           if (serverResponse.message) {
-//             throw new Error(serverResponse);
-//           }
-//           // open one more transaction
-//           const transaction = db.transaction(["new_pizza"], "readwrite");
-//           // access the new_pizza object store
-//           const pizzaObjectStore = transaction.objectStore("new_pizza");
-//           // clear all items in your store
-//           pizzaObjectStore.clear();
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_budget"], "readwrite");
+          // access the new_budget object store
+          const budgetObjectStore = transaction.objectStore("new_budget");
+          // clear all items in your store
+          budgetObjectStore.clear();
 
-//           alert("All saved pizza has been submitted!");
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-//     }
-//   };
-// }
+          alert("Saved budget has been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
 
-// // listen for app coming back online
-// window.addEventListener("online", uploadPizza);
+// listen for app coming back online
+window.addEventListener("online", uploadBudget);
